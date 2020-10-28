7e5707cba8b625af61630d4181d1cb8719af2513
Raghav Khator <rk9md@virgina.edu>
2020-10-27 22:11:33 -0400

    Finished all the tic tac toe comments

--- TicTacToe/ttt.py
+++ TicTacToe/ttt.py
@@ -1,28 +1,77 @@
 import sys
+'''
+This script takes in the string representation of a standard tic tac toe board 
+as a command line argument and returns the possible moves for the player whose
+turn it is. The script assumes the standard rules of tic tac toe 
+(X moves first, first player to three in a row wins, players alternate turns, etc). 
+
+The board is represented by a string of length 9 consisting of periods, X’s, and O’s.
+The periods represent empty spaces, the X’s represent where the X player has placed their piece,
+and the O represents where the O player has placed their piece. 
+Each spot on the board is also referred to by its index which is given as follows:
+0 1 2
+3 4 5
+6 7 8
+
+The moves are categorized into good, bad, and tie. 
+The “good” moves are the moves that lead to a win no matter what the opponent does. 
+The “bad” moves are the moves that lead to a loss no matter what the opponent does. 
+The “tie” moves are the rest of the moves (moves that could lead to either a win or a loss depending on the opponent). 
+
+'''
+
+# Number of rows/cols
 side = 3
+# Number of Total Spaces
 n = side**2
-turnDecider = n&1
+# List of the sets of indexes of the spaces in each row, column, and diagonal. 
+rowsColsDiags = [{0,1,2},{3,4,5}, {6,7,8}, {0,3,6},{1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}]
+# List of the list of indexes of the spaces in each row, column, and diagonal.
+listRowsColsDiags = [[*x] for x in rowsColsDiags]
+# The two possible symbols for the game of tic tac toe
+sym = ["X", "O"]
+
+# Checks if the string representation of the board is valid by 
+# ensuring its length is 9 and by making sure that the board 
+# either has the same number of X's and O's or has one more X.
+# Returns 1 for a valid board and 0 for an invalid board.
 def validBoard(pzl):
-    return pzl.count("X")-pzl.count("O") in {1,0} and len(pzl)==9
+    return pzl.count("X")-pzl.count("O") in {1,0} and len(pzl)==n
+
+# Determines whose turn turn it is. Odd number of periods means X turn
+# and even means O turn. Returns 0 for X and 1 for O.    
 def whoseTurn(pzl):
     return 1-pzl.count(".")&1
 
+# Prints out the puzzle as a 3x3 square.
 def display(pzl):
     [print(pzl[x:x+side]) for x in range(0,n,side)]
 
+# Returns the index of the empty spaces on the board.
 def emptySpaces(pzl):
     return [x for x in range(len(pzl)) if pzl[x]=="."]    
-rowsColsDiags = [{0,1,2},{3,4,5}, {6,7,8}, {0,3,6},{1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}]
-listRowsColsDiags = [[*x] for x in rowsColsDiags]
-#^^needs to be changed for any num of spaces
-sym = ["X", "O"]
-def gameOver(pzl): #needs to be changed for any num of spaces
+
+# Determines if a player won. Checks every row/column/diagonal if
+# there is 3 of the same piece. Returns true if a player won, returns false otherwise.
+def gameOver(pzl): 
     for x in listRowsColsDiags:
         if pzl[x[0]]==pzl[x[1]]==pzl[x[2]]!=".":
             return True
     return False
-    #return sum([pzl[x[0]]==pzl[x[1]]==pzl[x[2]]!="." for x in listRowsColsDiags])
 
+'''
+Recursive method that determines the good, bad, and tie moves for a given board.
+Two bases cases:
+If a player has won, return a non empty bad set, so the previous recursion can categorize the move as good for the previous player.
+If no one has won, return a non empty tie set, so the previous recursion can categorize the move as a tie move for the previous player.
+
+Recursive Case:
+Iterate through every possible move for the current player, create a new board as if the current player played at that spot, and recur
+on the new board for the opposite player. 
+If the recursion returns any moves that guarantee a win for the opponent, add the move the player hypothetically made into the bad category. 
+If the recursion returns any moves that cannot guarantee a win or a loss for the opponent, add the move the player hypothetically made into the tie category. 
+If the recursion returns any moves that guarantee a loss for the opponent, add the move the player hypothetically made into the good category. 
+'''
 def partitionMoves(pzl, turn, numSpaces):
     if gameOver(pzl):
         return set(),{""},set()
@@ -40,23 +89,28 @@ def partitionMoves(pzl, turn, numSpaces):
             good.add(x)
     return good, bad, tie    
 
+# Read in the commmand line parameter of the board.
 pzl = sys.argv[1].upper()
-#pzl = "."*n
-#pzl = "XOOX....."
+display(pzl)
+# Check if the board is valid or one player has already won.
+# If no one has won, the board is valid, and the game is not tied, run partitionMoves to find out the set of moves for the given board.
 if not validBoard(pzl):
     print("Invalid Starting Board")
 elif gameOver(pzl):
-    print("Game is already finished")
+    print("Game has already finished")
 else:
     who = whoseTurn(pzl)
-    display(pzl)
-    g,b,t = partitionMoves(pzl, who, len(emptySpaces(pzl)))
-    if not g:
-        g = " None "
-    if not b:
-        b = " None "
-    if not t:
-        t = " None "
-    print("Good moves: "+str(g)[1:-1])
-    print("Bad moves: "+str(b)[1:-1])
-    print("Tie moves: "+str(t)[1:-1])
+    empty = len(emptySpaces(pzl))
+    if empty == 0:
+        print("Game has already finished in a tie.")
+    else:
+        g,b,t = partitionMoves(pzl, who, empty)
+        if not g:
+            g = " None "
+        if not b:
+            b = " None "
+        if not t:
+            t = " None "
+        print("Good moves: "+str(g)[1:-1])
+        print("Bad moves: "+str(b)[1:-1])
+        print("Tie moves: "+str(t)[1:-1])
--- TicTacToe/tttgame.py
+++ TicTacToe/tttgame.py
@@ -1,31 +1,64 @@
 import sys, time, msvcrt
+'''
+
+
+'''
+
+# Number of rows/cols
 side = 3
+# Number of Total Spaces
 n = side**2
-turnDecider = n&1
+# List of the sets of indexes of the spaces in each row, column, and diagonal. 
+rowsColsDiags = [{0,1,2},{3,4,5}, {6,7,8}, {0,3,6},{1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}]
+# List of the list of indexes of the spaces in each row, column, and diagonal.
+listRowsColsDiags = [[*x] for x in rowsColsDiags]
+# The two possible symbols for the game of tic tac toe
+sym = ["X", "O"]
+
+# Checks if the string representation of the board is valid by 
+# ensuring its length is 9 and by making sure that the board 
+# either has the same number of X's and O's or has one more X.
+# Returns 1 for a valid board and 0 for an invalid board.
 def validBoard(pzl):
-    return pzl.count("X")-pzl.count("O") in {1,0} and len(pzl)==9
+    return pzl.count("X")-pzl.count("O") in {1,0} and len(pzl)==n
+
+# Determines whose turn turn it is. Odd number of periods means X turn
+# and even means O turn. Returns 0 for X and 1 for O.    
 def whoseTurn(pzl):
     return 1-pzl.count(".")&1
 
+# Prints out the puzzle as a 3x3 square.
 def display(pzl):
     [print(pzl[x:x+side]) for x in range(0,n,side)]
 
+# Returns a list of the index of the empty spaces on the board.
 def emptySpaces(pzl):
     return [x for x in range(len(pzl)) if pzl[x]=="."] 
+# Returns a list of the index of the empty spaces on the board as a string.  
 def emptySpacesStr(pzl):
-    return [str(x) for x in range(len(pzl)) if pzl[x]=="."] 
+    return [str(x) for x in range(len(pzl)) if pzl[x]=="."]
 
-rowsColsDiags = [{0,1,2},{3,4,5}, {6,7,8}, {0,3,6},{1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}]
-listRowsColsDiags = [[*x] for x in rowsColsDiags]
-#^^needs to be changed for any num of spaces
-sym = ["X", "O"]
-def gameOver(pzl): #needs to be changed for any num of spaces
+# Determines if a player won. Checks every row/column/diagonal if
+# there is 3 of the same piece. Returns true if a player won, returns false otherwise.
+def gameOver(pzl): 
     for x in listRowsColsDiags:
         if pzl[x[0]]==pzl[x[1]]==pzl[x[2]]!=".":
             return True
     return False
-    #return sum([pzl[x[0]]==pzl[x[1]]==pzl[x[2]]!="." for x in listRowsColsDiags])
 
+'''
+Recursive method that determines the good, bad, and tie moves for a given board.
+Two bases cases:
+If a player has won, return a non empty bad set, so the previous recursion can categorize the move as good for the previous player.
+If no one has won, return a non empty tie set, so the previous recursion can categorize the move as a tie move for the previous player.
+
+Recursive Case:
+Iterate through every possible move for the current player, create a new board as if the current player played at that spot, and recur
+on the new board for the opposite player. 
+If the recursion returns any moves that guarantee a win for the opponent, add the move the player hypothetically made into the bad category. 
+If the recursion returns any moves that cannot guarantee a win or a loss for the opponent, add the move the player hypothetically made into the tie category. 
+If the recursion returns any moves that guarantee a loss for the opponent, add the move the player hypothetically made into the good category. 
+'''
 def partitionMoves(pzl, turn, numSpaces):
     if gameOver(pzl):
         return set(),{""},set()
@@ -44,9 +77,9 @@ def partitionMoves(pzl, turn, numSpaces):
     return good, bad, tie    
 
 
-
-#pzl = "."*n
-#pzl = "XOOX....."
+# Returns the index of the best move possible for the given player and the given board.
+# Returns -1 for the an invalid board
+# Returns 10 for a finished game.
 def nextMove(pzl, who):
     if not validBoard(pzl):
         print("Invalid Board")
@@ -55,15 +88,21 @@ def nextMove(pzl, who):
         print("Game is already finished")
         return 10
     else:
-        #who = whoseTurn(pzl)
-
         g,b,t = partitionMoves(pzl, who, len(emptySpaces(pzl)))
         for x in [g,t,b]:
             if x:
                 return x.pop()
 
+
+# Takes in up to two command line arguments. 
+# The single character argument is set to the human's piece
+# The multicharacter argument is read in as the starting board for the game.
+# These arguments can be read in any order.
+# If no board is given, the default is the empty board.
+# If no human character is given, teh default is setting the computer to the player with the next move and the human to the other. 
 pzl = None
 human = None
+argCount = 0
 for x in sys.argv[1:]:
     if len(x)==1:
         x = x.upper()
@@ -74,19 +113,26 @@ for x in sys.argv[1:]:
         comp = 1-human
     else:
         pzl = x.upper()
+    argCount+=1
+    if argCount==2:
+        break
 
 if not pzl:
     pzl= "........."
-if human==None:  #MAKE COMPUTER WHO IS THE NEXT MOVE
+if human==None:
     comp = whoseTurn(pzl)
     human = 1-comp
 
 
+'''
+This code plays out a game of tic tac toe given the parameters in the command line. The human is asked for their turn and is expected to type the index of where they want to play.
+The computer will make its move automatically in response to the human. The current board will display after each move as well as the move the active player made. If the human
+plays an invalid move, then they will be reprompted until a correct move is given or the player's time is up.
 
+Once the game is finished, the script will print out the outcome of the game.
+'''
 turn = whoseTurn(pzl)
-#print(turn==human)
 if validBoard(pzl):
-
     if not gameOver(pzl):
         while True:
             display(pzl)
@@ -94,7 +140,7 @@ if validBoard(pzl):
             placement = 0
             if turn==human:
                 print("Your Move:",end=" ", flush=True)
-                timeout = 80
+                timeout = 60
                 startTime = time.time()
                 poss = emptySpacesStr(pzl)
                 
